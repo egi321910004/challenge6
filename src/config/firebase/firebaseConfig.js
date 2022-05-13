ddbb7f2e27afd927d8155093245b8678423a9241
egi rizki ramadani <https://gitlab.com/egi321910004/basic_git.git>
2022-05-13 23:12:04 +0700

    revisi

--- src/config/firebase/firebaseConfig.js
+++ src/config/firebase/firebaseConfig.js
@@ -1,15 +1,16 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from 'firebase/auth';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD5R8HeDjdnH6pOyyKMcjWubzmIKnj7m3M",
-  authDomain: "challenge-6-67a39.firebaseapp.com",
-  projectId: "challenge-6-67a39",
-  storageBucket: "challenge-6-67a39.appspot.com",
-  messagingSenderId: "319665601002",
-  appId: "1:319665601002:web:1ac11984d7a9ecdf3c5a93"
+  apiKey: "AIzaSyDQn4jZEOmDi-BVeNVNpROji0rzSmRQ-uw",
+  authDomain: "challenge06-2526c.firebaseapp.com",
+  projectId: "challenge06-2526c",
+  storageBucket: "challenge06-2526c.appspot.com",
+  messagingSenderId: "622631028406",
+  appId: "1:622631028406:web:3800052e55ff1b385b4817"
 };
 
 // Initialize Firebase
