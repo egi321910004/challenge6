7f43649682b617077f8a6555f15de1694a57f9ba
egi rizki ramadani <https://gitlab.com/egi321910004/basic_git.git>
2022-05-13 23:19:16 +0700

    revisi terakhir

--- src/config/firebase/firebaseConfig.js
+++ src/config/firebase/firebaseConfig.js
@@ -1,9 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from 'firebase/auth';
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDQn4jZEOmDi-BVeNVNpROji0rzSmRQ-uw",
   authDomain: "challenge06-2526c.firebaseapp.com",
@@ -12,6 +10,7 @@ const firebaseConfig = {
   messagingSenderId: "622631028406",
   appId: "1:622631028406:web:3800052e55ff1b385b4817"
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
